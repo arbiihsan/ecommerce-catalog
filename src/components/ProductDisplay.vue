5c60af3d04cff1a174d9d96b47b5f70c4f83ddcc
arbi <arbiihsan@gmail.com>
2023-06-05 10:18:45 +0700

    first commit

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -5,45 +5,96 @@
 
 <script>
 export default {
-    // name: ProductDisplay,
+    name: 'ProductDisplay',
     data() {
         return {
-            number: 1
+            isLoading: false,
+            index: 0,
+            isProductAvailable: false,
+            product: {}
         }
-    }
+    },
+    methods: {
+        async callAPI() {
+            const response = await fetch(`https://fakestoreapi.com/products/${this.index}`);
+            const result = await response.json();
+            return result;
+        },
+        async getSingleProduct() {
+            this.isLoading = true;
+
+            if (this.index !== 20) {
+                this.index++
+            } else {
+                this.index = 1;
+            }
+
+            let data = await this.callAPI()
+            if (data.category === "men's clothing" || data.category === "women's clothing") {
+                this.product = { data }
+                this.isProductAvailable = true;
+            } else {
+                this.isProductAvailable = false;
+            }
+
+            this.isLoading = false;
+        }
+    },
+    mounted() {
+        this.getSingleProduct();
+    },
 }
 </script>
 
 <template>
     <div class='page-background'>
+        <div v-if="isLoading" class="product-card">
+            <div class="product-loading">
+                <div class="loading-thumbnail"></div>
+                <div class="loading-details">
+                    <div class="loading-details-top"></div>
+                    <div class="loading-details-bottom"></div>
+                </div>
+            </div>
+        </div>
         <!-- product card -->
-        <div class="container">
+        <div v-else class="container">
+            <div v-if="!isProductAvailable" class="unavailable">
+                <div class="details">
+                    <p>This product is unavailable to show</p>
+                    <div class="cta">
+                        <button type="button" @click="getSingleProduct()" class="cta-next">Next Product</button>
+                    </div>
+                </div>
+            </div>
             <!-- product -->
-            <div class="product">
+            <div v-else class="product">
                 <!-- photo -->
-                <div class="photo">
-                    <img src="https://m.media-amazon.com/images/I/31VL11ccCdL._SS400_.jpg" alt="Product Photo">
+                <div class="photo" v-if="product && product.data && product.data.image">
+                    <div class="thumbnail">
+                        <img :src="product.data.image" alt="">
+                    </div>
                 </div>
                 <!-- description -->
-                <div class="description">
+                <div class="description" v-if="product && product.data">
                     <div class="title">
-                        <h2>Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket</h2>
+                        <h3 :class="product.data.category === 'men\'s clothing' ? 'font-navy' : 'font-magenta'" class="title">{{ product.data.title }}</h3>
                     </div>
                     <div class="category">
-                        <p>women's clothing</p>
+                        <span>{{ product.data.category }}</span>
                         <hr>
                     </div>
                     <div>
-                        <p>100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON</p>
+                        <p>{{ product.data.description }}</p>
                     </div>
                     <div class="priceproduct">
                         <hr>
                         <div class="price">
-                            <h1>$29.95</h1>
+                            <span :class="product.data.category === 'men\'s clothing' ? 'font-navy' : 'font-magenta'" class="price">${{ product.data.price }}</span>
                         </div>
-                        <div class="buttons">
-                            <button class="buy">Buy Now</button>
-                            <button class="next">Next Product</button>
+                        <div class="cta">
+                            <button type="button" :class="product.data.category === 'men\'s clothing' ? 'bg-navy' : 'bg-magenta'" class="cta-buy">Buy Now</button>
+                            <button type="button" @click="getSingleProduct()" :class="product.data.category === 'men\'s clothing' ? 'border-navy font-navy' : 'border-magenta font-magenta'" class="cta-next">Next Product</button>
                         </div>
                     </div>
                 </div>
@@ -51,3 +102,4 @@ export default {
         </div>
     </div>
 </template>
+
